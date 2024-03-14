53bcd27dc21e4c18725ef7d2cc4aa07db16ce1fe
ialej001 <ivan.alejandre@gmail.com>
2024-03-13 20:43:14 -0700

    updates from dev

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,35 @@
-export default function Button() {
-    return <button>Hello I do nothing</button>
+import { MouseEventHandler } from "react";
+
+interface ButtonProps {
+  buttonType: "submit" | "reset" | "button";
+  buttonText: string;
+  onClick?: MouseEventHandler | undefined;
+  size?: "sm" | "md" | "lg" | undefined;
+  color?: "primary" | "warning" | "cancel" | undefined;
+  icon?: React.ReactNode | undefined;
 }
 
+export const Button = (props: ButtonProps) => {
+  let buttonSchemeColor;
+
+  switch (props.color) {
+    case "primary":
+      buttonSchemeColor = "border-[#7981da]";
+    default:
+      buttonSchemeColor = "border-black";
+  }
+
+  let tailwindCSS = `w-full cursor-pointer rounded border ${buttonSchemeColor} px-10 py-3 font-bold text-[#7981da] hover:bg-[#5865F2] hover:text-white`;
+
+  return (
+    <button
+      type={props.buttonType}
+      onClick={props.onClick}
+      className={tailwindCSS}
+    >
+      <span className="flex flex-row items-center justify-center gap-3 text-xs sm:text-base">
+        {props.icon} {props.buttonText}
+      </span>
+    </button>
+  );
+};
--- src/pages/dash/index.tsx
+++ src/pages/dash/index.tsx
@@ -1,23 +1,27 @@
 import { useSession } from "next-auth/react";
 import React from "react";
+import { SideNav } from "~/components/dash/sideNav";
 
 function UserDash() {
   const { data: sessionData } = useSession();
 
   if (!sessionData) {
-    return <div>
-        Access denied.
-    </div>;
+    return <div>Access denied.</div>;
   }
 
-  return <section>
-    <div>Events near you</div>
-    
-    <div>My Events</div>
-    <div>carosel of public events?</div>
-    
-    <div>events that you currently registered to</div>
-  </section>
+  return (
+    <section className="flex gap-4">
+      <SideNav/>
+      <div className="flex flex-col gap-6">
+        <div className="text-2xl">Events near you</div>
+        <div>Carousel of events ranked nearest to furthest. Limit 10.</div>
+        <div className="text-2xl">My Hosted Events</div>
+        <div>Another carousel, this time hosted event for this user.</div>
+        <div className="text-2xl">Events currently registered</div>
+        <div className="text-2xl">Events currently registered</div>
+      </div>
+    </section>
+  );
 }
 
 export default UserDash;
