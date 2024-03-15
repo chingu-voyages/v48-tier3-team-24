53b4ca2b563ee56fc118f2b1c7af4a0ccc3c7881
ialej001 <ivan.alejandre@gmail.com>
2024-03-14 21:46:13 -0700

    resolved conflicts due to merges

--- src/pages/dash/index.tsx
+++ src/pages/dash/index.tsx
@@ -1,7 +1,8 @@
 import { useSession } from "next-auth/react";
 import React from "react";
-import EventList, { EventsType } from "~/components/dash/EventList";
+import { EventList } from "~/components/Event/EventList";
 import { SideNav } from "~/components/dash/SideNav";
+import demoStatusData from "~/pages/ui-components/event-components-demo/demo_data";
 
 function UserDash() {
   const { data: sessionData } = useSession();
@@ -10,50 +11,15 @@ function UserDash() {
     return <div>Access denied.</div>;
   }
 
-  const events1: EventsType[] = [
-    {
-      id: 1,
-      name: "event name 1",
-      description: "blurb 1",
-      type: "virtual"
-    },
-    {
-      id: 2,
-      name: "event name 2",
-      description: "blurb 2",
-      type: "virtual"
-    },
-    {
-      id: 3,
-      name: "event name 3",
-      description: "blurb 3",
-      type: "in_person"
-    },
-  ]
+  const events1 = demoStatusData.slice(0, 1);
+  const events2 = demoStatusData.slice(1, 3);
 
-  const events2: EventsType[] = [
-    {
-      id: 1,
-      name: "event name 4",
-      description: "blurb 1",
-      type: "virtual"
-    },
-    {
-      id: 2,
-      name: "event name 5",
-      description: "blurb 2",
-      type: "virtual"
-    },
-
-  ]
-  
   return (
     <section className="flex gap-4 p-6">
-      <SideNav/>
+      <SideNav />
       <div className="flex flex-col gap-6 pl-6">
-        <EventList title="Events near you" events={events1}/>
-        <EventList title="My Hosted Events" events={events2}/>
-        <EventList title="Upcoming Registered Events" />
+        <EventList events={events1} />
+        <EventList events={events2} />
       </div>
     </section>
   );
