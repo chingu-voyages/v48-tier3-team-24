e68193329aa58c1b8de78c862f2c29c5faf2375d
ialej001 <ivan.alejandre@gmail.com>
2024-03-15 00:12:24 -0700

    build clean up

--- src/components/Event/EventContainer.tsx
+++ src/components/Event/EventContainer.tsx
@@ -13,6 +13,7 @@ const EventContainer = ({ title, events }: EventContainerProps) => {
     <section className="container rounded-md border-2 border-black">
       <div className="align-center inline-flex h-12 w-full items-center justify-between rounded-md bg-slate-300 text-2xl">
         <span className="md:w-3/4 lg:w-5/6 pl-5">{title}</span>
+        {/* button would be dynamic eventually. If the # of events is <= 2, button won't render. */}
         <span className="md:w-1/4 lg:w-1/6">
           <Button variant="primary">View All</Button>
         </span>
