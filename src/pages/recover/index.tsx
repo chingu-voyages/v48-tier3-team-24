e723c3aef06b050a3f6ce72a03b9eae5c347e7a1
mpark4656 <mpark4656@gmail.com>
2024-03-16 00:37:02 -0400

    Implement password and username recovery

---
+++ src/pages/recover/index.tsx
@@ -0,0 +1,11 @@
+import RecoverForm from "~/components/recover/RecoverForm";
+
+const Recover = () => {
+  return (
+    <section>
+      <RecoverForm />
+    </section>
+  );
+};
+
+export default Recover;
