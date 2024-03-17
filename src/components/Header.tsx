3f5d7e6dd117ecbb50f65945592bf77a6c5ea143
Briantam0422 <briantam0422@gmail.com>
2024-03-16 19:23:15 -0700

    components: add Header layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import Button from "./Button";
+import { IoMdNotificationsOutline } from "react-icons/io";
+
+const Header = () => {
+  return (
+    <div className="fixed w-full px-40 py-10">
+      <div className="flex justify-between">
+        <div className="flex items-center gap-5">
+          <div>Logo</div>
+          <div>Search Bar</div>
+        </div>
+        <div className="flex items-center gap-8">
+          <Button size="sm" className="hidden md:block" outline="info">
+            Manage Event
+          </Button>
+          <IoMdNotificationsOutline className="text-3xl" />
+          <div className="h-10 w-10 rounded-full border bg-black text-white">
+            <p className="mt-2 text-center">W</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
