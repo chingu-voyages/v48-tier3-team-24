9ce2eceabdbd9d2a38cbbb5aaacef9e59fde54ff
Briantam0422 <briantam0422@gmail.com>
2024-03-16 22:03:55 -0700

    header: check login status & add login button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,24 @@ import Image from "next/image";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { MdOutlineLogout } from "react-icons/md";
 import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { api } from "~/utils/api";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+  const { data: sessionData } = useSession();
   const [openIconMenu, setOpenIconMenu] = useState(false);
-  const goTo = (route: string) => {
+
+  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined },
+  );
+
+  const goTo = async (route: string) => {
     /* TODO redirect to a page  */
     console.log("manage event button onClick", route);
+    await router.push(route);
   };
   const onClickIconMenu = () => {
     setOpenIconMenu(!openIconMenu);
@@ -49,36 +61,42 @@ const Header = () => {
             className="relative cursor-pointer text-3xl"
             onClick={() => goTo("/notification")}
           />
-          <div className="relative">
-            <div
-              className="h-10 w-10 cursor-pointer rounded-full border bg-black text-white hover:shadow-lg"
-              onMouseEnter={onClickIconMenu}
-            >
-              <p className="mt-2 text-center">W</p>
-            </div>
-            {openIconMenu && (
+          {sessionData ? (
+            <div className="relative">
               <div
-                className="absolute right-0 top-14 min-w-44 p-5 text-gray-700 shadow-lg"
-                onMouseLeave={onClickIconMenu}
+                className="h-10 w-10 cursor-pointer rounded-full border bg-black text-white hover:shadow-lg"
+                onMouseEnter={onClickIconMenu}
               >
-                <div
-                  className="hover:text-blue flex cursor-pointer items-center justify-between hover:text-cyan-600"
-                  onClick={() => goTo("/profile")}
-                >
-                  <FaRegCircleUser />
-                  <p>My Profile</p>
-                </div>
-                <hr className="my-3"></hr>
-                <div
-                  className="flex cursor-pointer items-center justify-between hover:text-cyan-600"
-                  onClick={logout}
-                >
-                  <MdOutlineLogout />
-                  <p>Logout</p>
-                </div>
+                <p className="mt-2 text-center">W</p>
               </div>
-            )}
-          </div>
+              {openIconMenu && (
+                <div
+                  className="absolute right-0 top-14 min-w-44 p-5 text-gray-700 shadow-lg"
+                  onMouseLeave={onClickIconMenu}
+                >
+                  <div
+                    className="hover:text-blue flex cursor-pointer items-center justify-between hover:text-cyan-600"
+                    onClick={() => goTo("/profile")}
+                  >
+                    <FaRegCircleUser />
+                    <p>My Profile</p>
+                  </div>
+                  <hr className="my-3"></hr>
+                  <div
+                    className="flex cursor-pointer items-center justify-between hover:text-cyan-600"
+                    onClick={logout}
+                  >
+                    <MdOutlineLogout />
+                    <p>Sign out</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div className="cursor-pointer" onClick={() => goTo("/login")}>
+              Sign In
+            </div>
+          )}
         </div>
         <div className="block text-3xl md:hidden">
           <RiMenu4Fill />
