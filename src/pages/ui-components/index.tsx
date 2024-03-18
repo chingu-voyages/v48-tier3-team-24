8f854ef48ef8e6e6c9b300a28bb5137c909b7d43
BrianTamDev <66020086+Briantam0422@users.noreply.github.com>
2024-03-17 20:47:45 -0700

    Add header compoent and search bar component (#49)
    add responsive header with hamburger menu

--- src/pages/ui-components/index.tsx
+++ src/pages/ui-components/index.tsx
@@ -4,19 +4,20 @@ import Link from "next/link";
 
 const UiComponents = () => {
   return (
-    <>
-      <div className="p-20">
-        <h1 className="mb-5 text-xl font-bold">Components List</h1>
-        <Link href="ui-components/button-demo">
-          <p className="text-[#2245c6]">1. Go to Button Component Demo</p>
-        </Link>
-        <Link href="ui-components/event-components-demo">
-          <p className="text-[#2245c6]">
-            2. Go to Event List & Event Card Component Demo
-          </p>
-        </Link>
-      </div>
-    </>
+    <div className="p-20">
+      <h1 className="mb-5 text-xl font-bold">Components List</h1>
+      <Link href="ui-components/button-demo">
+        <p className="text-[#2245c6]">1. Go to Button Component Demo</p>
+      </Link>
+      <Link href="ui-components/event-components-demo">
+        <p className="text-[#2245c6]">
+          2. Go to Event List & Event Card Component Demo
+        </p>
+      </Link>
+      <Link href="ui-components/header-demo">
+        <p className="text-[#2245c6]">3. Go to Header Demo</p>
+      </Link>
+    </div>
   );
 };
 
