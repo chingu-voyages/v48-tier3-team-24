63dae5cdf58a1d99670d5776ca091b2c6f88ad96
Briantam0422 <briantam0422@gmail.com>
2024-03-21 16:56:37 -0700

    create my profile form layout

---
+++ src/components/dash/my-profile/MyProfileForm.tsx
@@ -0,0 +1,51 @@
+import Button from "~/components/Button";
+import { TextInput } from "~/components/TextInput";
+
+const MyProfileForm = () => {
+  const onUpdateAccount = () => {
+    console.log("update");
+  };
+
+  const onDeleteAccount = () => {
+    console.log("delete");
+  };
+
+  return (
+    <>
+      <form className="grid grid-cols-1 gap-5">
+        <div className="rounded p-10 shadow">
+          <div className="grid grid-cols-2 grid-rows-1 gap-5">
+            <div className="col-span-2">
+              <h1 className="text-lg">Your Info</h1>
+              <hr className="mt-2"></hr>
+            </div>
+            <TextInput id="first_name" label="First Name" inputType="text" />
+            <TextInput id="last_name" label="Last Name" inputType="text" />
+            <TextInput id="name" label="Name (Discord)" inputType="text" />
+            <div className="col-span-2">
+              <h1 className="text-lg">Account Info</h1>
+              <hr className="mt-2"></hr>
+            </div>
+            <div className="col-span-2">
+              <TextInput id="email" label="Email" inputType="text" />
+            </div>
+            <TextInput id="username" label="Username" inputType="text" />
+            <TextInput id="password" label="Password" inputType="text" />
+            <div className="col-span-2">
+              <div className="flex w-full flex-wrap justify-between">
+                <Button variant="primary" onClick={onUpdateAccount}>
+                  Update
+                </Button>
+                <Button outline="danger" onClick={onDeleteAccount}>
+                  Delete Account
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default MyProfileForm;
--- src/pages/dash/my_profile/index.tsx
+++ src/pages/dash/my_profile/index.tsx
@@ -1,5 +1,23 @@
-export const MyProfile = () => {
-  return <div>My Profile</div>;
-};
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import MyProfileForm from "~/components/dash/my-profile/MyProfileForm";
+import Header from "~/components/Header";
+
+function MyProfile() {
+  const { data: sessionData } = useSession();
+
+  useEffect(() => {
+    console.log(sessionData);
+  });
+
+  return (
+    <>
+      <Header />
+      <div className="grid justify-center gap-5 px-20">
+        <MyProfileForm />
+      </div>
+    </>
+  );
+}
 
 export default MyProfile;
