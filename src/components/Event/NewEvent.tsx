7a90c2c8951e01c96f67d3d8786b68e221efe462
ialej001 <ivan.alejandre@gmail.com>
2024-03-21 01:14:21 -0700

    updated new event inputs

--- src/components/Event/NewEvent.tsx
+++ src/components/Event/NewEvent.tsx
@@ -1,28 +1,73 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import Button from "../Button";
 import { TextInput } from "../TextInput";
+import Datetime from "react-datetime";
+import moment, { Moment } from "moment";
 
 const NewEvent = () => {
-  const createNewEvent = () => {
+  const createNewEvent = (e: FormEvent) => {
+    e.preventDefault();
     alert("new event");
   };
+
+  // to disable days before today in date pickers
+  const yesterday = moment().subtract(1, "day");
+  const blockDays = (current: Moment) => current.isAfter(yesterday);
+
+  // set up the props to modify the datepicker input box
+  let datePickerInputProps = {
+    className: "border-2 rounded-md p-2 hover:shadow",
+  };
+
   return (
-    <form
-      onSubmit={createNewEvent}
-      className="flex h-full w-full flex-col items-center gap-5 sm:w-2/5"
-    >
-      <h1 className="text-3xl font-bold text-blue-800">New Event</h1>
-      <TextInput type="text" id="name" label="Name of the Event" required />
-      <TextInput
-        type="textArea"
-        id="description"
-        label="Describe your event:"
-        required
-      />
-      <Button type="submit" outline="primary">
-        Create
-      </Button>
-    </form>
+    <div className="container p-5">
+      <h1 className="mb-5 border-b-2 border-gray-500 text-3xl font-bold text-blue-800">
+        New Event
+      </h1>
+      <form
+        onSubmit={createNewEvent}
+        className="flex h-full w-full items-center gap-5"
+      >
+        <div className="flex flex-col gap-5">
+          <TextInput type="text" id="name" label="Name of the Event" required />
+          <TextInput
+            type="textArea"
+            id="description"
+            label="Describe your event:"
+            required
+          />
+          <TextInput type="text" id="imageUrl" label="Image URL" />
+          <TextInput
+            type="number"
+            id="price"
+            label="Price per participant"
+            min={0}
+          />
+        </div>
+        <div className="flex flex-col justify-start gap-5 px-5">
+          <div>
+            <label>When does your event begin?</label>
+            <Datetime
+              initialValue={moment()}
+              isValidDate={blockDays}
+              inputProps={datePickerInputProps}
+            />
+          </div>
+          <div>
+            <label>When does your event end?</label>
+            <Datetime
+              initialValue={moment().add(2, "hours")}
+              isValidDate={blockDays}
+              inputProps={datePickerInputProps}
+            />
+          </div>
+        </div>
+
+        <Button type="submit" outline="primary">
+          Create
+        </Button>
+      </form>
+    </div>
   );
 };
 
