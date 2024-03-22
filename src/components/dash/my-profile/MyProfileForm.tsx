3530ffeb0911ce5af1ea22da7e66ae9b165baeaf
Briantam0422 <briantam0422@gmail.com>
2024-03-21 17:23:14 -0700

    TBC: handle onSubmit

--- src/components/dash/my-profile/MyProfileForm.tsx
+++ src/components/dash/my-profile/MyProfileForm.tsx
@@ -1,4 +1,5 @@
 import { Session } from "next-auth";
+import { FormEvent } from "react";
 import Button from "~/components/Button";
 import { TextInput } from "~/components/TextInput";
 
@@ -7,8 +8,19 @@ interface myProfileFormProps {
 }
 
 const MyProfileForm = (props: myProfileFormProps) => {
-  const onUpdateAccount = () => {
-    console.log("update");
+  const onUpdateAccount = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const postData = {
+      first_name: formData.get("first_name"),
+      last_name: formData.get("last_name"),
+      name: formData.get("name"),
+      email: formData.get("email"),
+      username: formData.get("username"),
+      password: formData.get("password"),
+    };
+    // TODO call api
+    console.log("update", postData);
   };
 
   const onDeleteAccount = () => {
@@ -17,31 +29,62 @@ const MyProfileForm = (props: myProfileFormProps) => {
 
   return (
     <>
-      <form className="grid grid-cols-1 gap-5">
+      <form className="grid grid-cols-1 gap-5" onSubmit={onUpdateAccount}>
         <div className="rounded p-10 shadow">
           <div className="grid grid-cols-2 grid-rows-1 gap-5">
             <div className="col-span-2">
               <h1 className="text-lg">Your Info</h1>
               <hr className="mt-2"></hr>
             </div>
-            <TextInput id="first_name" label="First Name" inputType="text" />
-            <TextInput id="last_name" label="Last Name" inputType="text" />
-            <TextInput id="name" label="Name (Discord)" inputType="text" />
+            <TextInput
+              id="first_name"
+              label="First Name"
+              inputType="text"
+              required={true}
+            />
+            <TextInput
+              id="last_name"
+              label="Last Name"
+              inputType="text"
+              required={true}
+            />
+            <TextInput
+              id="name"
+              label="Name (Discord)"
+              inputType="text"
+              disable={true}
+              readonly={true}
+            />
             <div className="col-span-2">
               <h1 className="text-lg">Account Info</h1>
               <hr className="mt-2"></hr>
             </div>
             <div className="col-span-2">
-              <TextInput id="email" label="Email" inputType="text" />
+              <TextInput
+                id="email"
+                label="Email"
+                inputType="text"
+                disable={true}
+                readonly={true}
+              />
             </div>
-            <TextInput id="username" label="Username" inputType="text" />
+            <TextInput
+              id="username"
+              label="Username"
+              inputType="text"
+              required={true}
+            />
             <TextInput id="password" label="Password" inputType="text" />
             <div className="col-span-2">
               <div className="flex w-full flex-wrap justify-between">
-                <Button variant="primary" onClick={onUpdateAccount}>
+                <Button variant="primary" type="submit">
                   Update
                 </Button>
-                <Button outline="danger" onClick={onDeleteAccount}>
+                <Button
+                  outline="danger"
+                  onClick={onDeleteAccount}
+                  type="button"
+                >
                   Delete Account
                 </Button>
               </div>
