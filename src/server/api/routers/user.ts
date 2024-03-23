34aa208eae86daa94dba1ad4d9bcb9ab73e4e669
Briantam0422 <briantam0422@gmail.com>
2024-03-23 00:10:47 -0700

    TBC: adding user update api

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,9 +1,29 @@
+import { hash } from "~/utils/bcrypt";
+import { z } from "zod";
 import {
   createTRPCRouter,
+  protectedProcedure,
 } from "~/server/api/trpc";
+import { TRPCError } from "@trpc/server";
 
 
 
 export const userRouter = createTRPCRouter({
-  
+  update: protectedProcedure.input(z.object({
+    firstName: z.string().min(1).max(20),
+    lastName: z.string().min(1).max(20),
+    username: z.string().min(3).max(20),
+    password: z.string().min(8).max(20).transform(async (val) => await hash(val)),
+  })).mutation(async({ ctx, input }) => {
+    try {
+      return
+    } catch(error) {
+      if(error instanceof TRPCError) throw error;
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "An unexpected error occurred.",
+          cause: error
+        });
+    }
+  })
 });
