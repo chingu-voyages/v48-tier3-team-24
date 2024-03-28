ccb38987d6d98ce109ca33c4e91e3ebe3d18d1f3
mpark4656 <mpark4656@gmail.com>
2024-03-27 22:39:17 -0400

    Finished user filter panel

--- src/components/admin/users/UserFilter.tsx
+++ src/components/admin/users/UserFilter.tsx
@@ -1,6 +1,7 @@
-import type { FormEvent } from "react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import { useUserAdminContext } from "~/pages/admin/users";
 import { api } from "~/utils/api";
+import { IoMdSearch } from "react-icons/io";
 
 const UserFilter = () => {
   const { 
@@ -16,6 +17,7 @@ const UserFilter = () => {
       }, refetchOnWindowFocus: false
     }
   );
+  const [value, setValue] = useState<string> ("");
 
   const onSubmit = async (event:FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -25,24 +27,32 @@ const UserFilter = () => {
     setSearch(newSearch);
   };
 
+  const onInputChange = (event:ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  }
+
   const clearFilter = async () => {
     setPage(1);
+    setValue("");
     setSearch(null);
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <label>Filter By: </label>
-        <input type="text" name="search" />
+    <form onSubmit={onSubmit} className="flex flex-col justify-center items-center pt-3 gap-3">
+      <div className="flex flex-col relative">
+        <label htmlFor="search" className="text-xs">Filter By</label>
+        <input id="search" type="text" name="search" value={value} onChange={onInputChange} className="border border-es-primary px-3 py-1 pr-10" />
+        <button type="submit" className="absolute flex justify-center items-center right-0 top-4 text-white bg-es-primary w-[34px] h-[34px]">
+          <IoMdSearch />
+        </button>
       </div>
-      <div>
-        <button type="submit">Filter</button>
-        <button type="button" onClick={clearFilter}>Clear Filter</button>
-      </div>
-      <div>
-        Current Filter: {search}
-      </div>
+      { search &&
+        <div className="flex flex-col justify-center items-center">
+          <span onClick={clearFilter} title="Click to remove filter" className="border border-es-primary-light rounded-full px-2 py-1 text-white bg-es-primary-light text-xs cursor-pointer hover:font-bold">
+            {search}
+          </span>
+        </div>
+      }
     </form>
   );
 }
