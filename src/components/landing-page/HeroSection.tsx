636d266c2d2fd6d6e1a2b0bdbd2b13a5677be18c
Wendy Kuo <wendykuo@Wendys-MacBook-Air.local>
2024-03-31 20:25:24 -0700

    modified styles

--- src/components/landing-page/HeroSection.tsx
+++ src/components/landing-page/HeroSection.tsx
@@ -7,18 +7,18 @@ import Image from "next/image";
 export default function HeroSection() {
   const router = useRouter();
   const session = useSession();
- 
+
   const goTo = async (route: string) => {
     await router.push(route);
   };
   return (
     <>
-      <div className="grid md:grid-cols-2 grid-rows-1 p-20 pt-36">
+      <div className="grid md:grid-cols-2 grid-rows-1 p-20 pt-30">
         <div className="rounded text-wrap">
-        <h1 className="lg:text-5xl sm:text-3xl text-3xl sm:pr-0 md:pr-20 pb-3 font-black leading-relaxed">Find your tribe, explore interests, and create memorable experiences close to home</h1>
-        <h2 className="lg:text-2xl italic sm:pb-20 pb-20">Connect. Collaborate. Create: Your Community Hub</h2>
+          <h1 className="lg:text-5xl sm:text-3xl text-3xl sm:pr-0 md:pr-20 pb-3 font-black leading-relaxed">Find your tribe, explore interests, and create memorable experiences close to home</h1>
+          <h2 className="lg:text-2xl italic sm:pb-20 pb-20">Connect. Collaborate. Create: Your Community Hub</h2>
         </div>
-        <div className="self-center justify-self-center sm:pb-20">
+        <div className="self-center justify-self-center sm:pb-20 md:pb-0 lg:pb-0 pb-20">
           <Image
             src="/img/landing-page.png"
             width={750}
