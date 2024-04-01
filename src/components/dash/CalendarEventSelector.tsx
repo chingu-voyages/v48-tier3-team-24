391b7136be764654d27c3446f0601d94de626b71
ialej001 <ivan.alejandre@gmail.com>
2024-03-31 23:47:07 -0700

    init calendar event schema

---
+++ src/components/dash/CalendarEventSelector.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import Modal from "../Modal";
+import { CalendarEventsType } from "schemas";
+
+const CalendarEventSelector = ({
+  events,
+  toggle,
+}: {
+  events: CalendarEventsType;
+  toggle: boolean;
+}) => {
+  return (
+    <Modal modalOpen={toggle}>
+      {events.events.map((event) => (
+        <div></div>
+      ))}
+    </Modal>
+  );
+};
+
+export default CalendarEventSelector;
