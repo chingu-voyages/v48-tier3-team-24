e2a508e87a69f36eb973b032dd921d0691e2d8bf
Briantam0422 <briantam0422@gmail.com>
2024-04-12 01:21:42 -0700

    style problem

--- src/components/landing-page/HeroSection.tsx
+++ src/components/landing-page/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import Button from "~/components/Button";
 import Image from "next/image";
 
@@ -13,13 +13,32 @@ export default function HeroSection() {
   };
   return (
     <>
-      <div className="grid md:grid-cols-4 grid-rows-1 p-20 pt-30">
-        <div className="rounded text-wrap col-span-1">
-          <h1 className="lg:text-5xl sm:text-3xl text-3xl sm:pr-0 md:pr-20 pb-3 font-black leading-relaxed">Find your tribe, explore interests, and create memorable experiences close to home</h1>
-          <h2 className="lg:text-2xl italic sm:pb-20 pb-20">Connect. Collaborate. Create: Your Community Hub</h2>
-          <Button className="bg-gradient-to-r from-blue-900 to-green-500 text-white font-semibold py-2 px-4 rounded-md shadow-md hover:from-blue-600 hover:to-green-600" onClick={() => session.status === "authenticated" ? goTo("/dash") : goTo("/login")} outline="primary">Host an Event</Button>
+      <div className="pt-30 grid grid-rows-1 p-20 md:grid-cols-2">
+        <div className="col-span-1 text-wrap rounded">
+          <div className="flex h-full items-center justify-center">
+            <div>
+              <h1 className="pb-5 text-3xl font-black leading-relaxed sm:pr-0 sm:text-3xl md:pr-20 lg:text-5xl">
+                Find your tribe, explore interests, and create memorable
+                experiences close to home
+              </h1>
+              <h2 className="mb-10 italic lg:text-2xl">
+                Connect. Collaborate. Create: Your Community Hub
+              </h2>
+              <Button
+                className="rounded-md bg-gradient-to-r from-blue-900 to-green-500 px-4 font-semibold text-white shadow-md hover:from-blue-600 hover:to-green-600"
+                onClick={() =>
+                  session.status === "authenticated"
+                    ? goTo("/dash")
+                    : goTo("/login")
+                }
+                outline="primary"
+              >
+                Host an Event
+              </Button>
+            </div>
+          </div>
         </div>
-        <div className="self-center justify-self-center sm:pb-20 md:pb-0 lg:pb-0 pb-20 col-span-3">
+        <div className="col-span-1 self-center justify-self-center pb-20 sm:pb-20 md:pb-0 lg:pb-0">
           <Image
             src="/img/landing-page.png"
             width={750}
@@ -27,7 +46,6 @@ export default function HeroSection() {
             alt="Dashboard example"
           />
         </div>
-       
       </div>
     </>
   );
